afc84c6abe25b3a8d6ea723f89301ef6942bf4d0
Samara1404 <stambekova1404@gmail.com>
2023-11-08 21:49:18 +0600

    for_dart

---
+++ bin/for_dart.dart
@@ -0,0 +1,11 @@
+import 'package:for_dart/for_dart.dart' as for_dart;
+
+void main(List<String> arguments) {
+ 
+ int b = 1;
+ int c;
+ for (int a=1; a<10; a++){
+  c=a*b;
+  print('$a*$b = $c');
+ }
+}
