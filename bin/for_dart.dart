5707c08176a528b50ebe167d46537f94a5ef1a0f
Samara1404 <stambekova1404@gmail.com>
2023-11-09 22:32:56 +0600

    added for_dart

--- bin/for_dart.dart
+++ bin/for_dart.dart
@@ -2,10 +2,17 @@ import 'package:for_dart/for_dart.dart' as for_dart;
 
 void main(List<String> arguments) {
  
- int b = 1;
- int c;
- for (int a=1; a<10; a++){
-  c=a*b;
-  print('$a*$b = $c');
+//  int b = 1;
+//  int c;
+//  for (int a=1; a<10; a++){
+//   c=a*b;
+//   print('$a*$b = $c');
+  int c;
+  for (int a=1; a<10; a++){
+    for (int b=1; b<10; b++){
+      c=a+b;
+      print('$a*$b=$c');
+    }
+  }
  }
-}
+
